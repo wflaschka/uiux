233b114a91f220026587422ad10d2c3352badb93
wflaschka <flash@Flash-MPB15.local>
2017-09-13 12:02:44 -0500

    Prior to renaming branch, committing setup and early work for flashflex framework.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,31 @@
 // wflaschka 20170719 -- Revision for Semantic-UI
 // wflaschka 20170722 -- Revision for MaterializeCSS
-var gulp = require('gulp');
-var path = require('path');
-var concat = require('gulp-concat');
-var del = require('del');
-var relative = require('relative');
-var debug = require('gulp-debug');
-var runSequence = require('run-sequence');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var sassdoc = require('sassdoc');
-var util = require("gulp-util"); // https://github.com/gulpjs/gulp-util
-var log = util.log;
+// wflaschka 20170910 -- Revision for Spectre.less
 
-var less = require('gulp-less');
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
+var gulp                 = require('gulp');
+var path                 = require('path');
+var concat               = require('gulp-concat');
+var del                  = require('del');
+var relative             = require('relative');
+var debug                = require('gulp-debug');
+var runSequence          = require('run-sequence');
+var sass                 = require('gulp-sass');
+var sourcemaps           = require('gulp-sourcemaps');
+var autoprefixer         = require('gulp-autoprefixer');
+var sassdoc              = require('sassdoc');
+var util                 = require("gulp-util"); // https://github.com/gulpjs/gulp-util
+var log                  = util.log;
+
+var less                 = require('gulp-less');
+var cleancss             = require('gulp-clean-css');
+var csscomb              = require('gulp-csscomb');
+var rename               = require('gulp-rename');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
-var autoprefix= new LessPluginAutoPrefix({ browsers: ["last 4 versions"] });
-
-// https://semantic-ui.com/introduction/advanced-usage.html
-// var watch_semantic = require('./semantic/tasks/watch');
-// var build_semantic = require('./semantic/tasks/build');
+var autoprefix           = new LessPluginAutoPrefix({ browsers: ["last 4 versions"] });
 
 // Paths
-var rootTarget = 'dist/';
-var rootSource = 'src/';
+var rootTarget           = 'dist/';
+var rootSource           = 'src/';
 
 var buildTargets = {
 	'scripts'     : rootTarget + 'assets/scripts'
@@ -43,68 +41,30 @@ var buildTargets = {
 var paths = {
 	scripts: [
 		rootSource   + 'assets/scripts/**/*'
-		,'node_modules/nouislider/distribute/*.js'
-		,'node_modules/sweetalert2/dist/*.js'
+//		,'node_modules/nouislider/distribute/*.js'
+//		,'node_modules/sweetalert2/dist/*.js'
 	],
-	materializeJs: [
-		// Order is probably important.
-		// This ordering is from the MaterializeCSS gruntfile
-		 "node_modules/materialize-css/js/initial.js"
-		,"node_modules/materialize-css/js/jquery.easing.1.4.js"
-		,"node_modules/materialize-css/js/animation.js"
-		,"node_modules/materialize-css/js/velocity.min.js"
-		,"node_modules/materialize-css/js/hammer.min.js"
-		,"node_modules/materialize-css/js/jquery.hammer.js"
-		,"node_modules/materialize-css/js/global.js"
-		,"node_modules/materialize-css/js/collapsible.js"
-		,"node_modules/materialize-css/js/dropdown.js"
-		,"node_modules/materialize-css/js/modal.js"
-		,"node_modules/materialize-css/js/materialbox.js"
-		,"node_modules/materialize-css/js/parallax.js"
-		,"node_modules/materialize-css/js/tabs.js"
-		,"node_modules/materialize-css/js/tooltip.js"
-		,"node_modules/materialize-css/js/waves.js"
-		,"node_modules/materialize-css/js/toasts.js"
-		,"node_modules/materialize-css/js/sideNav.js"
-		,"node_modules/materialize-css/js/scrollspy.js"
-		,"node_modules/materialize-css/js/forms.js"
-		,"node_modules/materialize-css/js/slider.js"
-		,"node_modules/materialize-css/js/cards.js"
-		,"node_modules/materialize-css/js/chips.js"
-		,"node_modules/materialize-css/js/pushpin.js"
-		,"node_modules/materialize-css/js/buttons.js"
-		,"node_modules/materialize-css/js/transitions.js"
-		,"node_modules/materialize-css/js/scrollFire.js"
-		,"node_modules/materialize-css/js/date_picker/picker.js"
-		,"node_modules/materialize-css/js/date_picker/picker.date.js"
-		,"node_modules/materialize-css/js/date_picker/picker.time.js"
-		,"node_modules/materialize-css/js/character_counter.js"
-		,"node_modules/materialize-css/js/carousel.js"
-		,"node_modules/materialize-css/js/tapTarget.js"
-	],
+	materializeJs: [],
 	styles: [
 		rootSource    + 'assets/styles/**/*.css'
-		,'node_modules/nouislider/distribute/*.css'
+//		,'node_modules/nouislider/distribute/*.css'
 	],
 	fonts: [
 		rootSource    + 'assets/fonts/**/*'
-		,"node_modules/materialize-css/dist/fonts/**/*"
+//		,"node_modules/materialize-css/dist/fonts/**/*"
 	],
 	pdfs: [
 		rootSource    + 'assets/pdfs/**/*'
 	],
 	images: [
 		rootSource    + 'assets/images/**/*.*'
-		,'!' + rootSource + 'assets/images/**/*.psd'
-		,'!' + rootSource + 'assets/images/**/*.ai'
+//		,'!' + rootSource + 'assets/images/**/*.psd'
+//		,'!' + rootSource + 'assets/images/**/*.ai'
 	],
 	sass: [
-//		rootSource    + 'assets/styles/bulma.scss'
-		rootSource    + 'assets/styles/materialize/materialize.scss'
-//		rootSource    + 'assets/styles/site.scss'
+		rootSource    + 'assets/styles/site.scss'
 	],
 	less: [
-//		rootSource    + 'assets/styles/bulma.scss'
 	],
 	html: [
 		rootSource    + '**/*.htm?'
@@ -130,8 +90,6 @@ var sassdocOptions = {
 ////////////////////////////////////////////////////////////////////////////////////
 // Start work:
 log("Gulpfile building...")
-// log("--paths:"); log(paths);
-// log("--buildTargets:"); log(buildTargets);
 
 // Clean target directory of everything
 gulp.task('clean', function() {
@@ -221,12 +179,6 @@ gulp.task('materializeJs', function() {
 		.pipe(gulp.dest(buildTargets.scripts));
 });
 
-// import Semantic-UI tasks with custom task names
-// https://semantic-ui.com/introduction/advanced-usage.html
-// gulp.task('watch ui', 'Watch UI for Semantic UI', watch_semantic);
-// gulp.task('build ui', 'Build UI for Semantic UI', build_semantic);
-
-
 // Rerun the task when a file changes
 // invoke with: gulp watch
 gulp.task('watch', ['all'], function() {
@@ -249,38 +201,8 @@ gulp.task('all', function(callback) {
 	'images', 'scripts', 'styles',
 	'less', 'sass', 'fonts', 'pdfs',
 	'html', 'shell',
-	// 'build ui', // no semantic-ui
 	'materializeJs',
 	callback);
 });
 
-// gulp.task('default', ['watch', 'watch ui']); // no semantic-ui
 gulp.task('default', ['watch']);
-
-
-
-// https://github.com/Semantic-Org/Example-External-Gulpfile/blob/master/gulpfile.js
-// /*******************************
-//             Custom
-// *******************************/
-// 
-// var
-//   gulp         = require('gulp'),
-// 
-//   // require tasks as dependencies
-//   watch        = require('./semantic/tasks/watch'),
-//   build        = require('./semantic/tasks/build')
-// ;
-// 
-// 
-// /*******************************
-//              Tasks
-// *******************************/
-// 
-// 
-// gulp.task('watch-ui', watch);
-// gulp.task('build-ui', build);
-// 
-// // Gulp help descriptions also work
-// // gulp.task('watch-ui', 'Watch UI for Semantic UI', watch);
-// // gulp.task('build-ui', 'Build UI for Semantic UI', build);
