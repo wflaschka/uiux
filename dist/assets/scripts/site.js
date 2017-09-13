93d8829c19af4d8953d039b8cc56a2043a627859
wflaschka <flash@Flash-MPB15.local>
2017-09-13 12:02:44 -0500

    Prior to renaming branch, committing setup and early work for flashflex framework.

--- dist/assets/scripts/site.js
+++ dist/assets/scripts/site.js
@@ -1,27 +1,37 @@
+// https://stackoverflow.com/questions/7486309/how-to-make-script-execution-wait-until-jquery-is-loaded/17914854#17914854
+function defer(method) {
+    if (window.jQuery) {
+        method();
+    } else {
+        setTimeout(function() { defer(method) }, 100);
+    }
+}
+
+
 // Common JS utilities for site
 $(document).ready(function() {
 
-    // MaterializeCSS
-    // Update forms
-    Materialize.updateTextFields();
+    // // MaterializeCSS
+    // // Update forms
+    // Materialize.updateTextFields();
 
-    // Init dropdowns for header nav
-    $("nav .dropdown-user").dropdown();
+    // // Init dropdowns for header nav
+    // $("nav .dropdown-user").dropdown();
 
-    // Init side-nav for mobile
-    $(".button-collapse").sideNav();
+    // // Init side-nav for mobile
+    // $(".button-collapse").sideNav();
 
-    // Run AnimateCSS animation and remove when done
-    // https://github.com/daneden/animate.css
-    // $('div').animateCSS('bounce');
-    $.fn.extend({
-        animateCss: function (animationName) {
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            this.addClass('animated ' + animationName).one(animationEnd, function() {
-                $(this).removeClass('animated ' + animationName);
-            });
-            return this;
-        }
-    });
+    // // Run AnimateCSS animation and remove when done
+    // // https://github.com/daneden/animate.css
+    // // $('div').animateCSS('bounce');
+    // $.fn.extend({
+    //     animateCss: function (animationName) {
+    //         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    //         this.addClass('animated ' + animationName).one(animationEnd, function() {
+    //             $(this).removeClass('animated ' + animationName);
+    //         });
+    //         return this;
+    //     }
+    // });
 
 });
