b0f48bbaa66d965df675d891818cfd004f3adffa
wflaschka <flash@Flash-MPB15.local>
2017-09-21 12:25:28 -0500

    Added HTML template building

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // wflaschka 20170719 -- Revision for Semantic-UI
 // wflaschka 20170722 -- Revision for MaterializeCSS
 // wflaschka 20170910 -- Revision for Spectre.less
+// wflaschka 20170915 -- Revision for flashflex "Steal all the things!"
+// wflaschka 20170921 -- Revision for html templates
 
 var gulp                 = require('gulp');
 var path                 = require('path');
@@ -15,13 +17,14 @@ var autoprefixer         = require('gulp-autoprefixer');
 var sassdoc              = require('sassdoc');
 var util                 = require("gulp-util"); // https://github.com/gulpjs/gulp-util
 var log                  = util.log;
-
 var less                 = require('gulp-less');
 var cleancss             = require('gulp-clean-css');
 var csscomb              = require('gulp-csscomb');
 var rename               = require('gulp-rename');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var autoprefix           = new LessPluginAutoPrefix({ browsers: ["last 4 versions"] });
+var hbsmaster            = require('gulp-handlebars-master');
+var fs                   = require('fs');
 
 // Paths
 var rootTarget           = 'dist/';
@@ -58,8 +61,8 @@ var paths = {
 	],
 	images: [
 		rootSource    + 'assets/images/**/*.*'
-//		,'!' + rootSource + 'assets/images/**/*.psd'
-//		,'!' + rootSource + 'assets/images/**/*.ai'
+		,'!' + rootSource + 'assets/images/**/*.psd'
+		,'!' + rootSource + 'assets/images/**/*.ai'
 	],
 	sass: [
 		rootSource    + 'assets/styles/site.scss'
@@ -69,6 +72,17 @@ var paths = {
 	html: [
 		rootSource    + '**/*.htm?'
 	],
+	html_templates: {
+		 index: rootSource + 'templates/pages/index.html'
+		,layouts: rootSource + 'templates/layouts/'
+		,partials: rootSource + 'templates/partials/'
+		,pages: [
+			rootSource + 'templates/pages/**/*',
+			'!' + rootSource + 'templates/pages/index.html'
+		]
+		,components: rootSource + 'templates/components/**/*'
+		,component_partials: rootSource + 'templates/components/' // path used for including components into pages
+	},
 	shell: [
 		rootSource + '**/*.sh'
 	]
@@ -103,7 +117,7 @@ gulp.task('scripts', function() {
 });
 
 // Styles
-gulp.task('styles', function () {
+glp.task('styles', function () {
 	return gulp.src(paths.styles)
 		.pipe(gulp.dest(buildTargets.styles));
 });
@@ -118,7 +132,7 @@ gulp.task('sass', function () {
 		.src(paths.sass)
 		.pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
-		.pipe(sourcemaps.write('/maps'))
+	.pipe(sourcemaps.write('/maps'))
 		.pipe(gulp.dest(buildTargets.styles))
 		.pipe(sassdoc(sassdocOptions))
 		.resume();
@@ -137,9 +151,14 @@ gulp.task('pdfs', function() {
 });
 
 // HTML
-gulp.task('html', function() {
-	return gulp.src(paths.html)
-		.pipe(gulp.dest(buildTargets.html));
+gulp.task('html', function(callback) {
+//	return gulp.src(paths.html)
+//		.pipe(gulp.dest(buildTargets.html));
+  runSequence(
+	'build-html-index',
+	'build-html-pages',
+	'build-html-component-pages',
+	callback);
 });
 
 // FONTS
@@ -177,6 +196,50 @@ gulp.task('materializeJs', function() {
 			.pipe(concat('materialize.js'))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(buildTargets.scripts));
+});
+
+// TEMPLATING FUNCTIONALITY 20170921
+gulp.task('build-html-component-pages', function() {
+	// Master used for components:
+	var master = paths.html_templates.layouts + 'component-only.html';
+
+	// Options for generation, from:
+	// https://www.npmjs.com/package/gulp-compile-handlebars
+	options = {
+		ignorePartials: true, //ignores the unknown partials
+		// batch : Javascript array of filepaths to use as partials
+		batch : [paths.html_templates.partials]
+	}
+
+	return gulp.src(paths.html_templates.components)
+		.pipe( hbsmaster(master, [], options))
+		.pipe(gulp.dest(buildTargets.html + "components/"));
+});
+
+gulp.task('build-html-index', function() {
+	var master = paths.html_templates.layouts + 'project-index.html';
+	options = {
+		ignorePartials: true, //ignores the unknown partials
+		batch : [paths.html_templates.partials]
+	}
+	return gulp.src(paths.html_templates.index)
+		.pipe( hbsmaster(master, [], options))
+		.pipe(gulp.dest(buildTargets.html));
+});
+
+gulp.task('build-html-pages', function() {
+	var master = paths.html_templates.layouts + 'page.html';
+	options = {
+		ignorePartials: true, //ignores the unknown partials
+		batch : [
+			paths.html_templates.partials
+			,paths.html_templates.component_partials + "headers/"
+			,paths.html_templates.component_partials
+		]
+	}
+	return gulp.src(paths.html_templates.pages)
+		.pipe( hbsmaster(master, [], options))
+		.pipe(gulp.dest(buildTargets.html));
 });
 
 // Rerun the task when a file changes
